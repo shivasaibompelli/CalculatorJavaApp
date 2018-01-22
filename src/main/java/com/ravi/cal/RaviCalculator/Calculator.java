438e1e9003c6787f8e840f195740f2b809f8cb76
Ravi <ravi2krishna@gmail.com>
2018-01-22 18:01:40 +0530

    Update Calculator.java

--- src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
+++ src/main/java/com/ravi/cal/RaviCalculator/Calculator.java
@@ -25,7 +25,7 @@ public class Calculator {
 
 	public long addFucn(long first, long second){
 		
-		return first-second;
+		return first+second;
 	}
 	
 	public long subFucn(long first, long second){
